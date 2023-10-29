ea0d163817518a621d11a127de328e9b8ccaa25a
alienCarpet14 <danko.tomas.ts@gmail.com>
2023-10-29 02:41:13 +0200

    login and register API

---
+++ database_credentialsExample.js
@@ -0,0 +1,7 @@
+module.exports = {
+    USER: 'postgres',
+    HOST: 'localhost',
+    DATABASE: 'asos',
+    PASSWORD: '<PASSWORD>',   //change to your postres DB password
+    PORT: 2555     //change to whatever port number you like
+};
