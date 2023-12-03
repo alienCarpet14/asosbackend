9b2cea6ad99eb85024c572c2c78133083d06fb49
alienCarpet14 <danko.tomas.ts@gmail.com>
2023-12-03 11:33:40 +0100

    removed redundant files

--- database_credentialsExample.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    USER: 'postgres',
-    HOST: 'localhost',
-    DATABASE: 'asos',
-    PASSWORD: '<PASSWORD>',   //change to your postres DB password
-    PORT: 2555     //change to whatever port number you like
-};
